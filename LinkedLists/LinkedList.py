0cfacb39f8f421d00d4fefc94290ed3e27447013
PureJD <6454jonny@gmail.com>
2024-05-02 15:26:54 +0100

    Inital commit

---
+++ LinkedLists/LinkedList.py
@@ -0,0 +1,19 @@
+class Node():
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList():
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1 
+
+
+
+#Test code 
+test_linked_list = LinkedList(23)
+print(test_linked_list.tail.value)
+print(test_linked_list.tail.value)
+print(test_linked_list.length)
