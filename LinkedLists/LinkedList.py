6fb9403902705521903a45a59295b50c7ad2028e
PureJD <6454jonny@gmail.com>
2024-05-03 09:42:37 +0100

    push

--- LinkedLists/LinkedList.py
+++ LinkedLists/LinkedList.py
@@ -12,16 +12,35 @@ class LinkedList():
         self.tail = new_node
         self.length = 1 
 
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def append(self, value):
+        '''Method that will append a new node to the end of the current list'''
+        appended_node = Node(value)
+        if self.head is None:
+            self.head = appended_node
+            self.tail = appended_node
+        else:
+            self.tail.next = appended_node
+            self.tail = appended_node
+        self.length += 1
+        return True 
+
 
 
 #Test code 
 test_linked_list = LinkedList(23)
+second_link = LinkedList(24)
 print(test_linked_list.tail.value)
 print(test_linked_list.tail.value)
 print(test_linked_list.length)
 
 
-
+test_linked_list.print_list
 
 
 
