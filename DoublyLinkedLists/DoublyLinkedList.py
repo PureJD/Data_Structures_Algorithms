9ecc0770bd638f177a1490ba672422db86f6a8f3
PureJD <6454jonny@gmail.com>
2024-05-10 09:50:31 +0100

    Append function added

--- DoublyLinkedLists/DoublyLinkedList.py
+++ DoublyLinkedLists/DoublyLinkedList.py
@@ -20,5 +20,20 @@ class Doublylinkedlist():
             print(temp.value)
             temp = temp.next
 
+    def append(self, value):
+        '''This function will create a new node and point the next and previous values before moving the tail across to the end'''
+        new_node = Node(value)
+        if self.tail == None:
+            self.head = new_node
+            self.tail = new_node
+        new_node.prev = self.tail
+        self.tail.next = new_node
+        self.tail = new_node
+        
+        
+
 test_list = Doublylinkedlist(5)
-test_list.print_list()          
+test_list.print_list()
+print('Now a test for appending the value 6')
+test_list.append(6)
+test_list.print_list()        
