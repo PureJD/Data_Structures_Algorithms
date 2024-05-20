f7825487499fe03d1552d66945f4c804e637212a
Jon <6454jonny@gmail.com>
2024-05-20 06:05:45 +0100

    new method for adding a node to the end of the queue added

--- Stacks_Queues/queue.py
+++ Stacks_Queues/queue.py
@@ -19,8 +19,24 @@ class Queue:
             print(temp.value)
             temp = temp.next
             
+    def enqueue(self, value):
+        '''This function will add a new node to the end of the queue'''
+        new_node = Node(value)
+        if self.first is None:
+            self.first = new_node
+            self.last = new_node
+        else:
+            self.last.next = new_node
+            self.last = new_node
+        self.length += 1
+            
+         
+            
     
 my_queue = Queue(5)
+my_queue.print_queue()
+print('Add a new node to the queue')
+my_queue.enqueue(6)
 my_queue.print_queue()
 
         
