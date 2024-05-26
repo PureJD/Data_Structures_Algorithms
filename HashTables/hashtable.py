b73b05b51d61e5dbfa792c66b4dae40a1afa7f45
Jon <6454jonny@gmail.com>
2024-05-26 10:30:57 +0100

    hash table constructed

--- HashTables/hashtable.py
+++ HashTables/hashtable.py
@@ -1,9 +1,20 @@
 class HashTable:
+    '''The constructor for the hash table'''
     def __init__(self, size = 7):
         self.data_map = [None] * size
     
     def __hash(self, key):
+        '''This function will create a hash'''
         my_hash = 0
         for letter in key:
             my_hash = (my_hash + ord(letter) * 23) % len(self.data_map)
         return my_hash
+    
+    def print_table(self):
+        '''This function will print the table below'''
+        for i, val in enumerate(self.data_map):
+            print(i, ': ', val)
+            
+hash_table = HashTable()
+print('The below will print the hash table with all values currently set to None')
+hash_table.print_table() 
