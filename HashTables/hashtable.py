b44e8285445e09a125a45ab0b58421dde4e08f9f
Jon <6454jonny@gmail.com>
2024-06-03 15:04:41 +0100

    Additional information added to hash tables

--- HashTables/hashtable.py
+++ HashTables/hashtable.py
@@ -15,6 +15,29 @@ class HashTable:
         for i, val in enumerate(self.data_map):
             print(i, ': ', val)
             
+    def set_item(self, key, value):
+        '''This function will set the value in the hash table'''
+        index = self.__hash(key)
+        if self.data_map[index] is None:
+            self.data_map[index] = []
+        self.data_map[index].append([key, value])
+        
+    def get_item(self, key):
+        '''This function will get the value from the hash table'''
+        index = self.__hash(key)  
+        if self.data_map[index] is not None:
+            for i in range(len(self.data_map[index])):
+                if self.data_map[index][i][0] == key:
+                    return self.data_map[index][i][1]
+        return None
+            
 hash_table = HashTable()
 print('The below will print the hash table with all values currently set to None')
 hash_table.print_table() 
+print('The below will set the value of grapes to 10000')
+hash_table.set_item('grapes', 10000)
+hash_table.print_table()
+print('The below code will test the get_item function')
+print(hash_table.get_item('grapes'))
+print(hash_table.get_item('apples'))    
+
